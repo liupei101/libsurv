dd8244b49bbd59caff9e35d5a14838045adb9659
NUM_24 <1018810506@qq.com>
2019-06-24 10:58:52 +0800

    V2.0: Expose Interface.

--- libsurv/efnboost/__init__.py
+++ libsurv/efnboost/__init__.py
@@ -1,4 +1,7 @@
 from ._efn_core import efn_loss
 from .model import model
 
-__all__ = ["model", "efn_loss"]
+__ALL__ = [
+    'model',
+    'efn_loss',
+]
--- libsurv/hitboost/__init__.py
+++ libsurv/hitboost/__init__.py
@@ -2,4 +2,8 @@ from ._hit_core import hit_loss
 from ._hit_core import hit_tdci
 from .model import model
 
-__all__ = ["model", "hit_tdci", "hit_loss"]
+__all__ = [
+    "model", 
+    "hit_tdci", 
+    "hit_loss",
+]
